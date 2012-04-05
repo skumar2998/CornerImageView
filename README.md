4f6ffa142ae788f27db64b658dad2ffbfe8c7b21
DotDot <xinsuhui@gmail.com>
2012-04-05 11:03:21 +0800

    build project
    Signed-off-by: DotDot <xinsuhui@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# Corner ImageView
+ This is a android widget who can the corner to round
+
+## Features
+
+## How To Do
+
---
+++ widget/src/com/indexkey/view/widget/CornerImageView.java
@@ -0,0 +1,23 @@
+package com.indexkey.view.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class CornerImageView extends ImageView {
+
+	public CornerImageView(Context context)
+	{
+		super(context);
+	}
+
+	public CornerImageView(Context context, AttributeSet attrs) {
+		super(context, attrs, 0);
+	}
+
+	public CornerImageView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+}
